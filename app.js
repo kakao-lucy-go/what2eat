42ec7056039f92fbc136b5e8851834f820f667d6
mychum1 <want5617@gmail.com>
2020-05-31 15:08:31 +0900

    add gitignore, add post api for store

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var app = express();
-var fs = require('fs')
+var fs = require('fs');
+//var bodyParser = require('body-parser');
+
+
+// create application/json parser
+
+// create application/x-www-form-urlencoded parser
+//var urlencodedParser = bodyParser.urlencoded({ extended: false })
+ 
+//app.use(bodyParser.json({ type: 'application/*+json' }))
+//app.use(bodyParser().json())
+//app.use('/', router)
+app.use(express.json())
 
 var router = require('./router/main')(app, fs);
-
-
+ 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
--- router/main.js
+++ router/main.js
@@ -14,6 +14,28 @@ module.exports = function(app, fs)
         });
      });
 
+     app.post('/stores', function (req, res) {
+        var result = {  };
+        var store = req.body;
+
+        // LOAD DATA & CHECK DUPLICATION
+        fs.readFile( __dirname + "/../data/store.json", 'utf8',  function(err, data){
+            var stores = JSON.parse(data);
+            
+            stores.push(store)
+            console.log(store)
+            fs.writeFile(__dirname + "/../data/store.json",
+                            
+                JSON.stringify(stores, null, '\t'), "utf8", function(err, data){
+                    result = {"success": 1};
+                    res.json(result);
+                }
+            )
+
+        })
+
+     });
+
      app.get('/menus', function (req, res) {
         //__dirname : 현재 모듈 위치
         fs.readFile( __dirname + "/../data/" + "menu.json", "utf8", function (err, data) {
